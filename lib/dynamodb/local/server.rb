90caa7dd5dd3a75303d5be20cbd9364d8c059ec5
Pat Wilson <zerostride@gmail.com>
2021-01-20 11:13:36 -0800

    Fix args

--- lib/dynamodb/local/server.rb
+++ lib/dynamodb/local/server.rb
@@ -5,12 +5,12 @@ module Dynamodb
       DYNAMODB_LIB_DIR  = File.join(File.dirname(__FILE__), 'ext/DynamoDBLocal_lib')
       DYNAMODB_JAR_FILE = File.join(File.dirname(__FILE__), 'ext/DynamoDBLocal.jar')
 
-      def self.start(args)
-        exec "java -Djava.library.path=#{DYNAMODB_LIB_DIR} -jar #{DYNAMODB_JAR_FILE} #{ARGV.join(' ')}"
+      def self.start(args=ARGV)
+        exec "java -Djava.library.path=#{DYNAMODB_LIB_DIR} -jar #{DYNAMODB_JAR_FILE} #{args.join(' ')}"
       end
 
-      def self.start_background(args)
-        @pid = fork { exec "java -Djava.library.path=#{DYNAMODB_LIB_DIR} -jar #{DYNAMODB_JAR_FILE} #{ARGV.join(' ')}" }
+      def self.start_background(args=ARGV)
+        @pid = fork { exec "java -Djava.library.path=#{DYNAMODB_LIB_DIR} -jar #{DYNAMODB_JAR_FILE} #{args.join(' ')}" }
         Process.detach(@pid)
       end
 
